e5e24d9962404610c457eb52c20bb99f70e32901
Ozelea <67627754+Ozelea@users.noreply.github.com>
2020-08-31 17:55:23 +0500

    Update firstlesson.py

--- firstlesson.py
+++ firstlesson.py
@@ -1,10 +1,11 @@
 import tweepy
 import time
-consumer_key = "JPysYKuFx7mkYmXgRDef7CprE"
-consumer_secret = "0ZcxGmCQWugsBNPKqLC1j9W9zeBO0ghyUACDdRi1UQdbR4T0ki"
-access_key = "1195429060861669377-YVOvHfys7vMqPLlmazuw5B2s9eMbSn"
-access_secret = "FdMjgUZfGqvvj8leCc8tw2T42AkrRdoffsyus61ohqSNl"
+consumer_key = "XXXXXXXXXXXXXXXXXX"
+consumer_secret = "XXXXXXXXXXXXXXXXXXXXXX"
+access_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+access_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
 
+username = "Enter Your User Name"
 print("Script is Running.....")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -15,7 +16,7 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 
 file_name = "followers.txt"
 def store_followers(file_name):
-    followers = api.followers_ids("Abubakr160", [-1])
+    followers = api.followers_ids(username, [-1])
     file_write = open(file_name, "w")
     for id in followers:
         file_write.write(str(id)+'\n')
